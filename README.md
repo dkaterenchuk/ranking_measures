34b6af077c96beb70ae40a2be389e88a70704a0d
Denys Katerenchuk <denys.katerenchuk@gmail.com>
2017-01-17 12:55:02 -0500

    Adding all the files  Changes to be committed: 	modified:   README.md 	new file:   __init__.py 	new file:   contributors.txt 	new file:   examples.py 	new file:   measures.py

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ranking_measures
-RankDCG: ranking/ordering evaluation measure
+##Ranking/Ordering Evaluation Measures
+
+This package offers an implementation of the Rank Discounted Cumulative Gain (RankDCG) evaluation measure. This measure is designed to evaluate ranking-ordering algorithms. RankDCG had three fundamental properties: 1) consistent lower and upper score bounds, 2) it works with non-normal score distribution, and 3) it has transitivity property. For more detail see the paper (![RankDCG](http://www.dk-lab.com/wp-content/uploads/2014/07/RankDCG.pdf))
+
+Bellow is the list of all measures available in this package:
+
+* Rank Discounted Cumulative Gain (RankDCG) - a newly proposed measure t
+* Normalized Discounted Cumulative Gain (nDCG)
+* Discounted Cumulative Gain (DCG)
+* Average Precision (AP)
+* Mean Average Precision (MAP)
+
+NOTE: More ranking-ordering evaluation measures are available in sklearn and scipy packages. Scipy has an implementation of Kendall's tau (scipy.stats.kendalltau).
+
+##Installation
+Download the package and add ranking_measures directory to your python path or to your project folder.
+
+##Usage
+
+```python
+from ranking_measures import measures
+
+print measures.find_rankdcg([4,3,2,2,1], [3,4,2,2,1])
+```
+
+More example are in examples.py file.
---
+++ examples.py
@@ -0,0 +1,86 @@
+"""examples.py: an example code for running rank-ordering evaluation package
+=========================================================================
+
+ranking_eval is a set of common ranking algorithms such as:
+*dcg
+*ndcg
+*precision
+*precision_k
+*average_precision
+*rankdcg
+
+rankdcg is a new measure and it is discribed in this paper:
+DOTO: add link to rankDCG paper
+"""
+
+__author__ = "Denys Katerenchuk, The Graduate Center, CUNY"
+
+__license__ = """The MIT License (MIT)
+
+Copyright (c) [2015] [Denys Katerenchuk]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
+
+__version__ = "1.0.1"
+
+import measures
+
+
+
+def test_measures(reference, hypothesis):
+    """
+    Runs all rank-ordering evaluation measures on given pair of lists.
+    """
+
+    print "\t DCG:\t\t\t", measures.find_dcg(hypothesis)
+    print "\t nDCG:\t\t\t", measures.find_ndcg(reference, hypothesis)
+    print "\t Precision:\t\t", measures.find_precision(reference, hypothesis)
+    print "\t Precision at k:\t\t", measures.find_precision_k(reference, hypothesis, len(reference))
+    print "\t Average precision:\t", measures.find_average_precision(reference, hypothesis)
+    print "\t RankDCG:\t\t", measures.find_rankdcg(reference, hypothesis)
+
+#Defining test cases
+L1 = [9, 4, 4, 2, 2, 2, 1, 1, 1, 1]
+L2 = [9, 4, 4, 2, 2, 1, 2, 1, 1, 1]
+L3 = [4, 4, 2, 9, 2, 2, 1, 1, 1, 1]
+L4 = [1, 4, 4, 2, 2, 2, 9, 1, 1, 1]
+L5 = [1, 4, 4, 2, 2, 2, 1, 1, 1, 9]
+L6 = [1, 1, 1, 1, 2, 2, 2, 4, 4, 9]
+
+#Testing:
+print "1.Perfect ordering:"
+test_measures(L1, L1)
+
+print "2.Slightly wose case (low ranks):"
+test_measures(L1, L2)
+
+print "3. Further worse case (hight ranks):"
+test_measures(L1, L3)
+
+print "4.Placement of high rank element into low rank 'group':"
+test_measures(L1, L4)
+
+print "5.Case #4 but with further misplacement of hight rank element inside low rank 'group':"
+test_measures(L1, L5)
+
+print "6.The worst case (reverse ordering):"
+test_measures(L1, L6)
+
+
